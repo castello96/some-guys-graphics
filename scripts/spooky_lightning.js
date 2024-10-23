0a3c5a7fbeb2308ec6dc2357c1578ad75caafdfa
Nick Castello <ncastello@thefarmersdog.com>
2024-10-22 23:54:12 -0400

    Made the drops more realistic

--- scripts/spooky_lightning.js
+++ scripts/spooky_lightning.js
@@ -22,23 +22,22 @@ let ghosts = [];
 let numGhosts = 5;
 
 let bloodDrops = [];
-let numBloodDrops = 3;
 
 function setup() {
   createCanvas(canvasWidth, canvasHeight);
 
-  imageBounds.push(new ImageBoundaries(0, 120, 135));
-  imageBounds.push(new ImageBoundaries(10, 95, 155));
-  imageBounds.push(new ImageBoundaries(15, 90, 160));
-  imageBounds.push(new ImageBoundaries(20, 35, 165));
-  imageBounds.push(new ImageBoundaries(25, 30, 170));
-  imageBounds.push(new ImageBoundaries(50, 20, 185));
-  imageBounds.push(new ImageBoundaries(100, 68, 148));
-  imageBounds.push(new ImageBoundaries(150, 35, 193));
-  imageBounds.push(new ImageBoundaries(200, 15, 145));
-  imageBounds.push(new ImageBoundaries(250, 22, 125));
-  imageBounds.push(new ImageBoundaries(275, 40, 102));
-  imageBounds.push(new ImageBoundaries(295, 70, 90));
+  imageBounds.push(new ImageBoundaries(0, 120, 135, -1));
+  imageBounds.push(new ImageBoundaries(10, 95, 155, -1));
+  imageBounds.push(new ImageBoundaries(15, 90, 160, -1));
+  imageBounds.push(new ImageBoundaries(20, 35, 165, -1));
+  imageBounds.push(new ImageBoundaries(25, 30, 170, -1));
+  imageBounds.push(new ImageBoundaries(50, 20, 185, -1));
+  imageBounds.push(new ImageBoundaries(100, 68, 148, 1));
+  imageBounds.push(new ImageBoundaries(150, 35, 193, 0));
+  imageBounds.push(new ImageBoundaries(200, 15, 145, 1));
+  imageBounds.push(new ImageBoundaries(250, 22, 125, 1));
+  imageBounds.push(new ImageBoundaries(275, 40, 102, 0));
+  imageBounds.push(new ImageBoundaries(295, 70, 90, 1));
 
   for (bound of imageBounds) {
     let drop = new BloodDrop(bound, 0, random(0.005, 0.02));
@@ -48,7 +47,7 @@ function setup() {
   for (i = 0; i < numGhosts; i++) {
     let x = int(random(0, canvasWidth));
     let y = int(random(0, canvasHeight));
-    let z = int(random(0, 100));
+    let z = int(random(0, 500));
     ghosts.push(new Ghost(x, y, z, 100, 100, ghostImage));
   }
 }
@@ -60,7 +59,7 @@ function mousePressed() {
 // Lightning
 let lightingFrequencyInFrames = 200;
 let lightningFlash = false;
-let lightningDuration = 25;
+let lightningDuration = 200;
 let lightningTimer = 0;
 
 function draw() {
@@ -112,8 +111,10 @@ function drawBlood() {
 }
 
 function drawLightningBolt(a) {
-  stroke(255, 255, 255, a);
-  strokeWeight(3);
+  noFill(); // Prevents any unwanted fill color
+  stroke(255, 255, 255, a); // Set stroke to white with opacity
+  strokeWeight(3); // Set stroke weight for the bolt
+
   // Draw a jagged lightning bolt using random points
   beginShape();
   let startX = random(100, 300);
@@ -157,36 +158,90 @@ class Ghost {
 }
 
 class BloodDrop {
-  constructor(path, speed, acceleration) {
-    this.path = path;
-    this.x = path.x;
-    this.y = path.top;
+  constructor(boundary, speed, acceleration, swayDirection) {
+    this.boundary = boundary; // Store the boundary
+    this.x = boundary.x;
+    this.y = boundary.top;
     this.speed = speed;
     this.acceleration = acceleration;
+    this.stuck = false; // Whether the drop is "hanging" at the bottom
+    this.swayDirection = -1 * boundary.bottomSlope; // Direction for swaying (left or right)
+    this.swayAmount = 0; // How much the drop sways
+    this.hangTime = 15; // Number of frames to "hang" before falling
+    this.isFalling = true; // If the drop is falling (not stuck)
+    this.hasFallenOff = false; // Whether the drop has already fallen after swaying
   }
 
   display() {
-    stroke(0); // Set the outline color to black
-    strokeWeight(1); // Set the outline thickness
+    if (this.isFalling && !this.hasFallenOff) {
+      // If falling, apply acceleration and make the drop move down
+      this.speed += this.acceleration;
+      this.y += this.speed;
+
+      // Check if the drop hits the bottom of the boundary
+      if (this.y >= this.boundary.bottom) {
+        // Stick to the bottom and start swaying
+        this.y = this.boundary.bottom;
+        this.isFalling = false;
+        this.stuck = true;
+      }
+    } else if (this.stuck) {
+      // Sway the drop back and forth
+      this.swayAmount += this.swayDirection * 0.25;
+
+      // Decrease hang time
+      this.hangTime--;
+
+      // Once hang time is over, start falling again
+      if (this.hangTime <= 0) {
+        this.stuck = false;
+        this.isFalling = true;
+        this.hasFallenOff = true; // Mark that the drop has fallen off
+        this.speed = 1; // Reset speed to avoid abrupt drops
+        this.hangTime = 15; // Reset hang time for future cycles
+      }
+    }
+
+    // If the drop has fallen off, apply acceleration and continue falling
+    if (this.isFalling && this.hasFallenOff) {
+      this.speed += this.acceleration;
+      this.y += this.speed;
+    }
+
+    // Display the drop
+    fill(150, 0, 0); // Main blood drop color
+    stroke(0);
+    strokeWeight(1);
+    ellipse(this.x + this.swayAmount, this.y, 5, 10); // Main blood drop shape
+
+    // Add shimmer or shine effect
+    noStroke();
+    fill(255, 150); // A light color with some transparency for the shine
+    ellipse(this.x + this.swayAmount - 1, this.y - 2, 2, 4); // Small vertical shimmer near the top of the drop
+
+    // Reset fill and stroke for future drawings
+    stroke(0);
     fill(150, 0, 0);
-    ellipse(this.x, this.y, 5, 10);
 
-    this.speed += this.acceleration;
-    this.y += this.speed;
-
+    // If the drop falls past the canvas height, reset it
     if (this.y > canvasHeight) {
-      this.y = this.path.top;
+      this.y = this.boundary.top;
       this.speed = 0;
+      this.isFalling = true; // Reset to falling state
+      this.hasFallenOff = false; // Reset fall status
+      this.hangTime = 15; // Reset hang time
+      this.swayAmount = 0; // Reset sway
     }
   }
 }
 
 class ImageBoundaries {
-  constructor(x, top, bottom) {
+  constructor(x, top, bottom, bottomSlope) {
     let X_OFFSET = imgX + 2;
     let Y_OFFSET = imgY;
     this.x = x + X_OFFSET;
     this.top = top + Y_OFFSET;
     this.bottom = bottom + Y_OFFSET;
+    this.bottomSlope = bottomSlope;
   }
 }
